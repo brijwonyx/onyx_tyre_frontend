4cb8d0b00715fa8d2e77d0b388daaf1d1bd851a4
Tushar1211111 <tushar.bhatt@precisebh.com>
2026-01-21 00:08:40 +0530

    just setup only

--- src/Home/home-controller.ts
+++ src/Home/home-controller.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const useHomeController = () => {
     const [text, setText] = useState("Hello")
-    return{
+    return {
         text,
         setText
     }
