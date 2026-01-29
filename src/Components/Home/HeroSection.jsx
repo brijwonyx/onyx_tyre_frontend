09f33239c15131ae6936ecb883dd2d9f53a2868b
UI-dev <ui@mywonyx.com>
2026-01-29 20:13:57 +0530

    image alignment fixed

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -4,7 +4,7 @@ import carImage from "../../assets/hero.png";
 const HeroSection = () => {
   return (
     <section className="bg-gradient-green px-[200px] py-20 relative">
-      <img src={carImage} alt="Car Image" className="absolute right-[-25px] top-0 "/>
+      <img src={carImage} alt="Car Image" className="absolute right-0 top-0 w-[calc(42%-20px)]"/>
 
       <div >
         <div>
