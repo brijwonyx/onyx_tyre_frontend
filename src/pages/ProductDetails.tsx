bd448dd2e5566572216564f713e4984a23074279
Sanyam <sanyamshama.tech@gmail.com>
2026-04-01 10:26:27 +0530

    [Feature] : Add Loader Product Detail

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -10,32 +10,53 @@ import ProductServices from "../components/productDetails/ProductServices";
 import Reviews from "../Components/productDetails/reviews/Reviews";
 import useProductDetailsController from "./product-details.controller";
 
+import ShimmerCard from "../Components/Common/Forms/ShimmerCard";
+import NoTyreState from "../Components/Common/layout/NoStockAvailable";
+
 const ProductDetails = () => {
   const location = useLocation();
 
-  const {productId} = location.state || {};
+  const { productId } = location.state || {};
 
-  const {productDetails , loading} = useProductDetailsController({id: productId});
+  const { productDetails, loading } = useProductDetailsController({
+    id: productId,
+  });
+
+  console.log(loading, "Product Details");
 
   return (
     <>
-      <ContextBar
+      {/* <ContextBar
         breadCrumbs="Home / by brand / Michelin / Michelin Pilot Sport 4"
         children=""
-      />
-    {
-      productDetails?.map((item)=>(
-      <div className="py-10 px-16" key={item.id}>
-        <ProductHero reviews={item.tyreModel.reviews} price={item?.inventories?.[0]?.price || 0} tyreDetails={item.tyreModel} />
-        <ProductServices className="!px-0" />
-        <ProductOverview />
-        <Reviews reviews={item.tyreModel.reviews}  />
-        <FAQs />
-        <GetYourTyres variant="dark" />
-      </div>
-    
-    ))
-    }
+      /> */}
+      {!loading ? (
+        <div className="space-y-6 p-8">
+          {[...Array(1)].map((_, i) => (
+            <ShimmerCard key={i} />
+          ))}
+        </div>
+      ) : productDetails?.length === 0 ? (
+        <div className="text-center p-20 text-gray-500">
+          {/* No tyres found for this selection... */}
+          <NoTyreState />
+        </div>
+      ) : (
+        productDetails?.map((item) => (
+          <div className="py-10 px-16" key={item.id}>
+            <ProductHero
+              reviews={item.tyreModel.reviews}
+              price={item?.inventories?.[0]?.price || 0}
+              tyreDetails={item.tyreModel}
+            />
+            <ProductServices className="!px-0" />
+            <ProductOverview />
+            <Reviews reviews={item.tyreModel.reviews} />
+            <FAQs />
+            <GetYourTyres variant="dark" />
+          </div>
+        ))
+      )}
       <FindTyreBanner />
     </>
   );
